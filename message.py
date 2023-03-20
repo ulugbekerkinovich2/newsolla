2358c493af44acdeea5322884dce1778dc5275d8
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2023-03-20 14:21:31 +0500

    'test7'

--- message.py
+++ message.py
@@ -1,7 +1,7 @@
 import asyncio
 import datetime
 
-
+from data.config import ADMINS
 from loader import bot
 
 
@@ -9,7 +9,7 @@ async def my_func():
     now = datetime.datetime.now()
     print(now.weekday())
 
-    if now.weekday() == 0 and now.hour == 14 and now.minute == 33:
+    if now.weekday() == 0 and now.hour == 14 and now.minute == 37:
         chat_id = 935920479
         name = 'Ulugbek'
         link = 'https://www.youtube.com/watch?v=jSfbVGb6lnw&list=RDMMjSfbVGb6lnw&start_radio=1'
@@ -19,6 +19,7 @@ async def my_func():
 async def main():
     while True:
         print(1)
+        bot.send_message(chat_id=ADMINS, text=1)
         await my_func()
         await asyncio.sleep(60)  # sleep for 1 minute
 
