045c6b9be8cbe289898eb2d12f291e17f9b1c395
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2023-04-04 12:48:15 +0500

    'ad'

--- data/config.py
+++ data/config.py
@@ -7,9 +7,9 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-# BOT_TOKEN = os.environ.get('TOKEN')  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-# ADMINS = os.environ.get("ADMINS")  # adminlar ro'yxati
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+BOT_TOKEN = os.environ.get('TOKEN')  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+ADMINS = os.environ.get("ADMINS")  # adminlar ro'yxati
 # IP = env.str("ip")  # Xosting ip manzili
 #
