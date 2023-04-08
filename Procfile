47df5b2347670e7eb4e2e888f5972d7c111c868e
ulugbekerkinovich2 <ulugbek9015y@gmail.com>
2023-04-08 07:39:12 +0500

    'changes2'

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 worker: python app.py
-worker1: python message.py
---
+++ db.py
@@ -0,0 +1,32 @@
+# import requests
+#
+# url = "https://api.apilayer.com/exchangerates_data/convert?to=UZS&from=USD&amount=1"
+#
+# payload = {}
+# headers= {
+#   "apikey": "U7CXXvkaDQ3zUsFs6I1lfDnCIiQcWGsV"
+# }
+#
+# response = requests.request("GET", url, headers=headers, data = payload)
+#
+# status_code = response.status_code
+# result = response.text
+# print(result)
+
+import requests
+import json
+
+url = "https://api.apilayer.com/scraper?url=https%3A%2F%2Fopenbudget.uz%2F"
+
+headers = {
+    "apikey": "U7CXXvkaDQ3zUsFs6I1lfDnCIiQcWGsV"
+}
+
+response = requests.get(url, headers=headers)
+status_code = response.status_code
+result = json.loads(response.text)
+
+# Get the "text" value from the "data" dictionary
+text = result["data"]["text"]
+print(text)
+'4633ab3ae1b0408e83ca88f8095c6230'
